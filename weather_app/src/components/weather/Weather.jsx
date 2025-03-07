bbee4ef480274605abc5cc99c009d56b4aea1f35
Md-razvi <mraza1748@gmail.com>
2025-03-07 13:09:18 +0530

    Updated Project

--- weather_app/src/components/weather/Weather.jsx
+++ weather_app/src/components/weather/Weather.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect,useState,useRef } from 'react'
 import './Weather.css'
 import { CiSearch } from "react-icons/ci";
 import { SiAccuweather } from "react-icons/si";
@@ -6,12 +6,22 @@ import IconCard from '../icon-card/IconCard';
 import { WiHumidity } from "react-icons/wi";
 import { FaWind } from "react-icons/fa6";
 const Weather = () => {
+  const inputRef=useRef()
   const [weatherState,setweatherState]=useState(false)
   const search=async(city)=>{
+    if(city===""){
+      alert("Please Enter a city name")
+      return;
+    }
     try{
       const URL=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}&units=metric`
       const response=await fetch(URL);
       const data=await response.json()
+      if(!response.ok)
+      {
+        alert (data.message)
+        return;
+      }
       console.log(data)
       setweatherState({
         humidity:data.main.humidity,
@@ -21,7 +31,8 @@ const Weather = () => {
         icon:`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
       })
     }catch(error){
-        console.log(error)
+        setweatherState(false)
+        console.error("Error in fetching the data")
     }
   }
   useEffect(()=>{
@@ -31,17 +42,21 @@ const Weather = () => {
   return (
     <div className='weather'>
       <div className="search-bar">
-        <input type="text" className="styles" placeholder='Search' />
-        <CiSearch  size={55}  className='search'/> 
+        <input type="text" ref={inputRef} className="styles" placeholder='Search' />
+        <CiSearch  size={55}  className='search' onClick={()=>{ search(inputRef.current.value)}}/> 
       </div>
-      {image}
-      {/* <img src={weatherState.icon} className='weather-icon' alt={<SiAccuweather size={120} color={'yellow'}/>} /> */}
-      <p className='temperature'>{weatherState.temperature}°C</p>
-      <p className="location">{weatherState.location}</p>
-      <div className="weather-data">
-        <IconCard  icon={<WiHumidity size={40} />} temp={`${weatherState.humidity} %`} title={'Humidity'}/>
-        <IconCard  icon={<FaWind size={40}/>} temp={`${weatherState.windSpeed} km/h`} title={'Wind'}/>
-      </div>
+      {weatherState ? 
+  <>
+    {image}
+    <p className="temperature">{weatherState.temperature}°C</p>
+    <p className="location">{weatherState.location}</p>
+    <div className="weather-data">
+      <IconCard icon={<WiHumidity size={40} />} temp={`${weatherState.humidity} %`} title="Humidity" />
+      <IconCard icon={<FaWind size={40} />} temp={`${weatherState.windSpeed} km/h`} title="Wind" />
+    </div>    
+  </>
+ : <></>}
+      
     </div>
   )
 }
